d256a9e879a8d85e3f399b5fb05f3f0235666abb
Aditya <adiparab14@gmail.com>
2018-08-20 07:17:36 +0200

    Updated structure

---
+++ src/test/java/restassured/EncodeImage.java
@@ -0,0 +1,29 @@
+package restassured;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class EncodeImage {
+
+  public static String encodeFileToBase64Binary(File file) {
+    String encodedfile = null;
+    try {
+      FileInputStream fileInputStreamReader = new FileInputStream(file);
+      byte[] bytes = new byte[(int) file.length()];
+      fileInputStreamReader.read(bytes);
+      encodedfile = Base64.encodeBase64(bytes).toString();
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    return encodedfile;
+  }
+
+}
