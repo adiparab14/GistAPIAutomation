bc861534703a1dca937cfbfc808fae1104d0b656
Aditya <adiparab14@gmail.com>
2019-07-20 20:45:48 +0200

    Added rate_limit test cases

--- src/test/java/restassured/TestCases.java
+++ src/test/java/restassured/TestCases.java
@@ -1,6 +1,6 @@
 package restassured;
-import static io.restassured.RestAssured.given;
 
+import static io.restassured.RestAssured.given;
 
 import java.io.File;
 
@@ -13,112 +13,107 @@ import io.restassured.response.Response;
 
 public class TestCases {
 
-  final String baseURI = "https://api.github.com/";
-  String gistId;
-  File createGist = new File("src/main/resources/CreateGist.json");
-  
-  @BeforeSuite
-  public void setUp() {
+	final String baseURI = "https://api.github.com/";
+	final String token = "1f124ac1357fcbd65fc0b8c6338b91f05eeb30fd";
+	String gistId;
+	File createGist = new File("src/main/resources/CreateGist.json");
+	File editGist = new File("src/main/resources/EditGist.json");
 
-    RestAssured.baseURI = baseURI;
-    
+	@BeforeSuite
+	public void setUp() {
 
-  }
+		RestAssured.baseURI = baseURI;
 
-  @Test
-  public void noAuthenctication_readGistsAll() {
+	}
 
-    		given().
-    		when().
-    			get("/gists").
-    		then().
-    		assertThat().statusCode(200);
-  }
+	@Test
+	public void noAuthenctication_readGistsAll() {
 
-  @Test
-  public void noAuthenctication_readGistsSpecific() {
-	  
-	 Response response = 
-			 given().
-			 when().
-			 	get("/gists/437b031138d0d816eb00c39480e38224").
-			 then().
-			 	assertThat().statusCode(200).extract().response();
-	 
-	 Assert.assertEquals(response.path("id"),"437b031138d0d816eb00c39480e38224");
-	  }
-  
-  @Test
-  public void noAuthenctication_createGist() {
-	  
-	 //Response response = 
-			 given().
-			 when().
-			 	body(createGist).post("/gists").
-			 then().
-			 	assertThat().statusCode(401).extract().response();
-	  }
-  
-  @Test
-  public void noAuthenctication_editGist() {
-	  
-	 //Response response = 
-			 given().
-			 when().
-			 	body(createGist).patch("/gists/"+gistId).
-			 then().
-			 	assertThat().statusCode(404).extract().response();
-	  }
-  
-  @Test
-  public void noAuthenctication_deleteGist() {
-	  
-	 //Response response = 
-			 given().
-			 when().
-			 	body(createGist).delete("/gists/"+gistId).
-			 then().
-			 	assertThat().statusCode(404).extract().response();
-	  }
-  
-  @Test (priority = 0)
-  public void withAuthenctication_createGist() {
-	  
-	 Response response = 
-			 given().
-			 	auth().oauth2("c3d7ac635de7e52abe94b09be697160842e5df0c").
-			 when().
-			 	body(createGist).post("/gists").
-			 then().
-			 	assertThat().statusCode(201).extract().response();
-	 
-	  gistId = response.path("id");
-	  }
-  
-  @Test (priority = 1)
-  public void withAuthenctication_editGist_Negative() {
-	  
-	 Response response = 
-			 given().
-			 	auth().oauth2("c3d7ac635de7e52abe94b09be697160842e5df0c").
-			 when().
-			 	body(createGist).patch("/gists/"+gistId).
-			 then().
-			 	assertThat().statusCode(200).extract().response();
-	 Assert.assertEquals(response.path("id"),gistId);
-	  }
-  
-  @Test (priority = 2)
-  public void noAuthenctication_deleteGist_Negative() {
-	  
-	  Response response = 
-			  given().
-			  	auth().oauth2("c3d7ac635de7e52abe94b09be697160842e5df0c").
-			  when().
-			  	body(createGist).delete("/gists/"+gistId).
-			  then().assertThat().statusCode(204).extract().response();
-	  Assert.assertEquals(response.asString(),"");
-	  }
+		given().when().get("/gists").then().assertThat().statusCode(200);
+	}
 
+	@Test
+	public void noAuthenctication_readGistsSpecific() {
+
+		Response response = given().when().get("/gists/437b031138d0d816eb00c39480e38224").then().assertThat()
+				.statusCode(200).extract().response();
+
+		Assert.assertEquals(response.path("id"), "437b031138d0d816eb00c39480e38224");
+	}
+
+	@Test
+	public void noAuthenctication_createGist() {
+
+		given().when().body(createGist).post("/gists").then().assertThat().statusCode(401).extract().response();
+	}
+
+	@Test
+	public void noAuthenctication_editGist() {
+
+		given().when().body(editGist).patch("/gists/" + gistId).then().assertThat().statusCode(404).extract()
+				.response();
+	}
+
+	@Test
+	public void noAuthenctication_deleteGist() {
+
+		given().when().body(createGist).delete("/gists/" + gistId).then().assertThat().statusCode(404).extract()
+				.response();
+	}
+
+	@Test
+	public void noAuthenctication_rateLimit() {
+
+		Response response = given().when().get("/rate_limit").then().assertThat().statusCode(200).extract().response();
+		Assert.assertEquals(response.path("rate.limit").toString(), "60");
+	}
+
+	@Test
+	public void withAuthenctication_rateLimit() {
+
+		Response response = given().auth().oauth2(token).when().get("/rate_limit").then().assertThat().statusCode(200)
+				.extract().response();
+		Assert.assertEquals(response.path("rate.limit").toString(), "5000");
+	}
+
+	@Test
+	public void withAuthenctication_readGistsAll() {
+
+		given().auth().oauth2(token).when().get("/gists").then().assertThat().statusCode(200);
+	}
+
+	@Test
+	public void withAuthenctication_readGistsSpecific() {
+
+		Response response = given().auth().oauth2(token).when().get("/gists/437b031138d0d816eb00c39480e38224").then()
+				.assertThat().statusCode(200).extract().response();
+
+		Assert.assertEquals(response.path("id"), "437b031138d0d816eb00c39480e38224");
+	}
+
+	@Test(priority = 0)
+	public void withAuthenctication_createGist() {
+
+		Response response = given().auth().oauth2(token).when().body(createGist).post("/gists").then().assertThat()
+				.statusCode(201).extract().response();
+
+		gistId = response.path("id");
+	}
+
+	@Test(priority = 1)
+	public void withAuthenctication_editGist() {
+
+		Response response = given().auth().oauth2(token).when().body(editGist).patch("/gists/" + gistId).then()
+				.assertThat().statusCode(200).extract().response();
+		Assert.assertEquals(response.path("id"), gistId);
+	}
+
+	@Test(priority = 2)
+	public void withAuthenctication_deleteGist() {
+
+		Response response = given().auth().oauth2(token).when().delete("/gists/" + gistId).then().assertThat()
+				.statusCode(204).extract().response();
+		Assert.assertEquals(response.asString(), "");
+	}
 
 }
