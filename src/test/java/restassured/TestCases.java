6bc2db57ecf2ede0fd0c9452abb77b18bb5e9ca6
Aditya <adiparab14@gmail.com>
2019-07-20 12:11:24 +0200

    Updated .gitignore

--- src/test/java/restassured/TestCases.java
+++ src/test/java/restassured/TestCases.java
@@ -1,8 +1,6 @@
 package restassured;
-import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
-import static io.restassured.config.MultiPartConfig.multiPartConfig;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 
 import java.io.File;
 
@@ -11,120 +9,116 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class TestCases {
 
-  final String baseURI = "https://private-538b1b-verificationsrv.apiary-mock.com/";
-  String webhookParam = "webhook_url";
-  String webhookValue = "www.abc.com";
-  String refNumberParam = "reference_number";
-  String refNumberValue = "abc123";
-  String uriVerification, uriImage, uriId, id;
-
-
+  final String baseURI = "https://api.github.com/";
+  String gistId;
+  File createGist = new File("src/main/resources/CreateGist.json");
+  
   @BeforeSuite
   public void setUp() {
 
     RestAssured.baseURI = baseURI;
-
-    Response response = given().contentType(ContentType.JSON).when().get("").then().assertThat()
-        .statusCode(200).extract().response();
-
-    uriVerification = response.path("verifications_url");
+    
 
   }
 
-  @Test (groups ={"positive"})
-  public void createVerifications() {
+  @Test
+  public void noAuthenctication_readGistsAll() {
 
-    Response response = given().contentType(ContentType.JSON).when()
-        .param(webhookParam, webhookValue).param(refNumberParam, refNumberValue).post(uriVerification).then()
-        .assertThat().statusCode(201).body(matchesJsonSchemaInClasspath("Verifications.json")).extract().response();
-
-    uriImage = response.path("verification_images_url");
-    uriId = response.path("verification_url");
-    id = uriId.substring(uriId.lastIndexOf("/") + 1);
+    		given().
+    		when().
+    			get("/gists").
+    		then().
+    		assertThat().statusCode(200);
   }
 
+  @Test
+  public void noAuthenctication_readGistsSpecific() {
+	  
+	 Response response = 
+			 given().
+			 when().
+			 	get("/gists/437b031138d0d816eb00c39480e38224").
+			 then().
+			 	assertThat().statusCode(200).extract().response();
+	 
+	 Assert.assertEquals(response.path("id"),"437b031138d0d816eb00c39480e38224");
+	  }
+  
+  @Test
+  public void noAuthenctication_createGist() {
+	  
+	 //Response response = 
+			 given().
+			 when().
+			 	body(createGist).post("/gists").
+			 then().
+			 	assertThat().statusCode(401).extract().response();
+	  }
+  
+  @Test
+  public void noAuthenctication_editGist() {
+	  
+	 //Response response = 
+			 given().
+			 when().
+			 	body(createGist).patch("/gists/"+gistId).
+			 then().
+			 	assertThat().statusCode(404).extract().response();
+	  }
+  
+  @Test
+  public void noAuthenctication_deleteGist() {
+	  
+	 //Response response = 
+			 given().
+			 when().
+			 	body(createGist).delete("/gists/"+gistId).
+			 then().
+			 	assertThat().statusCode(404).extract().response();
+	  }
+  
+  @Test (priority = 0)
+  public void withAuthenctication_createGist() {
+	  
+	 Response response = 
+			 given().
+			 	auth().oauth2("c3d7ac635de7e52abe94b09be697160842e5df0c").
+			 when().
+			 	body(createGist).post("/gists").
+			 then().
+			 	assertThat().statusCode(201).extract().response();
+	 
+	  gistId = response.path("id");
+	  }
+  
+  @Test (priority = 1)
+  public void withAuthenctication_editGist_Negative() {
+	  
+	 Response response = 
+			 given().
+			 	auth().oauth2("c3d7ac635de7e52abe94b09be697160842e5df0c").
+			 when().
+			 	body(createGist).patch("/gists/"+gistId).
+			 then().
+			 	assertThat().statusCode(200).extract().response();
+	 Assert.assertEquals(response.path("id"),gistId);
+	  }
+  
+  @Test (priority = 2)
+  public void noAuthenctication_deleteGist_Negative() {
+	  
+	  Response response = 
+			  given().
+			  	auth().oauth2("c3d7ac635de7e52abe94b09be697160842e5df0c").
+			  when().
+			  	body(createGist).delete("/gists/"+gistId).
+			  then().assertThat().statusCode(204).extract().response();
+	  Assert.assertEquals(response.asString(),"");
+	  }
 
-  @Test(groups ={"negative"})
-  public void createVerificationInvalidWebhook() {
 
-    given().contentType(ContentType.JSON).when().param(webhookParam, "----")
-        .param(refNumberParam, refNumberValue).post(uriVerification).then().assertThat().statusCode(400)
-        .body(matchesJsonSchemaInClasspath("Error.json")).extract().response();
-
-  }
-
-  @Test(groups ={"positive"})
-  public void startVerificationCorrectID() {
-
-    Response response = given().contentType(ContentType.JSON).when().put("verifications/7878787").then().assertThat()
-        .statusCode(200).body(matchesJsonSchemaInClasspath("Verification_Status.json")).extract().response();
-
-    Assert.assertEquals(response.path("id"), id);
-
-  }
-
-  @Test (groups ={"negative"})
-  public void startVerificationIncorrectRessource() {
-
-    given().contentType(ContentType.JSON).when().put(uriId).then().assertThat().statusCode(400)
-        .body(matchesJsonSchemaInClasspath("Error.json")).extract().response();
-
-  }
-
-
-  @Test(groups ={"positive"})
-  public void uploadImageCorrectId() {
-
-    String encodedFile =
-        EncodeImage.encodeFileToBase64Binary(new File(getClass().getClassLoader().getResource("test.jpg").getFile()));
-
-    Response response = given().contentType("multipart/form-data").when()
-        .config(config().multiPartConfig(multiPartConfig().defaultSubtype("mixed").defaultBoundary("--BOUNDARY")))
-        .multiPart("type", "front", "text-plain").multiPart("auto_start", "false", "text/plain")
-        .multiPart("file", encodedFile, "image/jpeg").post(uriImage).then().assertThat().statusCode(202)
-        .body(matchesJsonSchemaInClasspath("Verification_AddImage.json")).extract().response();
-
-    Assert.assertEquals(response.path("message"), "Image received");
-
-  }
-
-  @Test(groups ={"negative"})
-  public void uploadImageInvalid() {
-
-    String encodedFile =
-        EncodeImage.encodeFileToBase64Binary(new File(getClass().getClassLoader().getResource("test.jpg").getFile()));
-
-    given().contentType("multipart/form-data").when()
-        .config(config().multiPartConfig(multiPartConfig().defaultSubtype("mixed").defaultBoundary("--BOUNDARY")))
-        .multiPart("type", "front", "text-plain").multiPart("auto_start", "false", "text/plain")
-        .multiPart("file", encodedFile, "image/jpeg").post(uriImage).then().assertThat().statusCode(400)
-        .body(matchesJsonSchemaInClasspath("Error.json")).extract().response();
-
-
-  }
-
-  @Test(groups ={"positive"})
-  public void downloadReportCorrectId() {
-
-
-    given().contentType(ContentType.JSON).when().get(uriVerification + "/" + id + "/report").then()
-        .assertThat().statusCode(200).extract().response();
-
-
-  }
-
-  @Test(groups ={"negative"})
-  public void downloadReportInCorrectId() {
-
-
-    given().contentType(ContentType.JSON).when().get(uriVerification + "/" + "ABCD" + "/report").then()
-        .assertThat().statusCode(400).body(matchesJsonSchemaInClasspath("Error.json")).extract().response();
-
-
-  }
 }
